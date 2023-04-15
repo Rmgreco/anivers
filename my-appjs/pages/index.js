3d829c25b9ffbb2ef2f4181e1ed9f25f9472ae67
Rmgreco <rafaelmgreco@hotmail.com>
2023-04-15 20:55:21 -0300

    change style

--- my-appjs/pages/index.js
+++ my-appjs/pages/index.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className={styles.title}>Aniver</h1>
+      <h1 className={styles.title}>Aniversário Aninha e Greco</h1>
     </main>
   )
 }
